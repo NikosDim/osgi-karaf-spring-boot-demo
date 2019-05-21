9a570abaefce15844814fab987301a6bb7fb9be8
nikos <nick.dimizas@gmail.com>
2019-05-21 15:52:21 +0100

    Added scr feature enablement and registration of the component bean

--- osgi-spring-boot-demo-bundle/src/main/java/com/nemesis/osgi/springboot/AppConfig.java
+++ osgi-spring-boot-demo-bundle/src/main/java/com/nemesis/osgi/springboot/AppConfig.java
@@ -1,6 +1,8 @@
 package com.nemesis.osgi.springboot;
 
 import com.nemesis.osgi.springboot.controller.ControllerConfig;
+import com.nemesis.osgi.springboot.service.ComponentService;
+import com.nemesis.osgi.springboot.service.impl.ComponentServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -12,5 +14,10 @@ public class AppConfig {
     @Bean
     public AppProperties appProperties() {
         return new AppProperties();
+    }
+
+    @Bean
+    public ComponentService componentService() {
+        return new ComponentServiceImpl();
     }
 }
